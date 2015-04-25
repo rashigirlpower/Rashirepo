f7d4714a44dd17be776ded31519d0a2a0a6afb4e
rashigirlpower <rashigirlpower@github.com>
2015-04-25 18:41:24 +0530

    First Commit

---
+++ calcee.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+oper = input("Enter operation")
+if oper == '+':
+	print("Sum =",a+b)
+elif oper == '-':
+	print("Substraction =",a-b)
+elif oper == '*':
+	print("Multiplication =",a*b)
+elif oper == '/':
+	print("Division =",a/b)
